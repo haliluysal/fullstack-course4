abac2116842f5ca7200d19b9319a770401aefb7c
haliluysal <haliluysal@gmail.com>
2021-06-07 07:19:18 -0400

    week 4

--- examples/Lecture41/js/script.js
+++ examples/Lecture41/js/script.js
@@ -5,11 +5,12 @@ var a = function () {
   var message = "inside a";
   console.log("a: message = " + message);
 
-  function b () {
-    console.log("b: message = " + message); 
-  }
-
   b();
+
+
 }
 
 a();
+function b () {
+  console.log("b: message = " + message); 
+}
--- examples/Lecture45/js/script.js
+++ examples/Lecture45/js/script.js
@@ -1,20 +1,20 @@
 // Object creation
-var company = new Object();
-company.name = "Facebook";
-company.ceo = new Object();
-company.ceo.firstName = "Mark";
-company.ceo.favColor = "blue";
+// var company = new Object();
+// company.name = "Facebook";
+// company.ceo = new Object();
+// company.ceo.firstName = "Mark";
+// company.ceo.favColor = "blue";
 
-console.log(company);
-console.log("Company CEO name is: " 
-  + company.ceo.firstName);
+// console.log(company);
+// console.log("Company CEO name is: " 
+//   + company.ceo.firstName);
 
-console.log(company["name"]);
-var stockPropName = "stock of company";
-company[stockPropName] = 110;
+// console.log(company["name"]);
+// var stockPropName = "stock of company";
+// company[stockPropName] = 110;
 
-console.log("Stock price is: " + 
-  company[stockPropName]);
+// console.log("Stock price is: " + 
+//   company[stockPropName]);
 
 // Better way: object literal
 var facebook = {
@@ -26,14 +26,4 @@ var facebook = {
   "stock of company": 110
 };
 
-console.log(facebook.ceo.firstName);
-
-
-
-
-
-
-
-
-
-
+console.log(facebook);
